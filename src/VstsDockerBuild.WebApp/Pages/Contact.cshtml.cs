c30c343401f4ff640425a4655f078f59d706120a
sparreio <christian@sparre.it>
2018-03-27 09:25:42 +0200

    Added another "service" to the solution

--- src/VstsDockerBuild.WebApp/Pages/Contact.cshtml.cs
+++ src/VstsDockerBuild.WebApp/Pages/Contact.cshtml.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace VstsDockerBuild.WebApp.Pages
 {
     public class ContactModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public ContactModel(IConfiguration configuration, HttpClient client)
+        {
+            _configuration = configuration;
+            _client = client;
+        }
+
         public string Message { get; set; }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-            Message = "Your contact page.";
+            var r = await _client.GetStringAsync(_configuration["PongService"]);
+
+            Message = $"My Ping! Your {r}";
         }
     }
 }
--- src/VstsDockerBuild.WebApp/Startup.cs
+++ src/VstsDockerBuild.WebApp/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,14 +14,15 @@ namespace VstsDockerBuild.WebApp
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(s => new HttpClient());
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
